71fba425f203ed69487f5cd0f02287fd01e3cc0f
Scott Jackson <daneren2005@gmail.com>
2015-09-22 15:19:08 -0700

    Remove stunnel since it seemed to have broken it

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # heroku-buildpack-twemproxy
-Run twemproxy and stunnel in a dyno along with your application 
+Run twemproxyin a dyno along with your application
 https://github.com/twitter/twemproxy
--- bin/gen-twemproxy-conf.sh
+++ bin/gen-twemproxy-conf.sh
@@ -3,31 +3,12 @@
 REDIS_URLS=${TWEMPROXY_URLS:-REDISCLOUD_URL}
 n=1
 
-# Enable this option to prevent stunnel failure with Amazon RDS when a dyno resumes after sleeping
-if [ -z "${ENABLE_STUNNEL_AMAZON_RDS_FIX}" ]; then
-  AMAZON_RDS_STUNNEL_OPTION=""
-else
-  AMAZON_RDS_STUNNEL_OPTION="options = NO_TICKET"
-fi
-
-mkdir -p /app/vendor/stunnel/var/run/stunnel/
-cat >> /app/vendor/stunnel/stunnel-twemproxy.conf << EOFEOF
-foreground = yes
-
-options = NO_SSLv2
-options = SINGLE_ECDH_USE
-options = SINGLE_DH_USE
-socket = r:TCP_NODELAY=1
-options = NO_SSLv3
-${AMAZON_RDS_STUNNEL_OPTION}
-ciphers = HIGH:!ADH:!AECDH:!LOW:!EXP:!MD5:!3DES:!SRP:!PSK:@STRENGTH
-EOFEOF
-
 for REDIS_URL in $REDIS_URLS
 do
   echo "Setting ${REDIS_URL}_TWEMPROXY config var"
   eval REDIS_URL_VALUE=\$$REDIS_URL
 
+  # redis://rediscloud:UV0d4ikbK7UIegq5@pub-redis-19886.us-east-1-1.2.ec2.garantiadata.com:19886
   DB=$(echo ${REDIS_URL_VALUE} | perl -lne 'print "$1 $2 $3 $4 $5 $6" if /^redis(?:ql)?:\/\/([^:]+):([^@]+)@(.*?):(.*?)(\\?.*)?$/')
   DB_URI=( $DB )
   DB_USER=${DB_URI[0]}
@@ -48,15 +29,7 @@ ${REDIS_URL}:
    - ${DB_HOST}:${DB_PORT}:1
 EOFEOF
 
-  cat >> /app/vendor/stunnel/stunnel-twemproxy.conf << EOFEOF
-[${REDIS_URL}]
-client = yes
-accept  = /tmp/.s.REDIS.620${n}
-connect = ${DB_HOST}:${DB_PORT}
-EOFEOF
-
   let "n += 1"
 done
 
 chmod go-rwx /app/vendor/twemproxy/*
-chmod go-rwx /app/vendor/stunnel/*
